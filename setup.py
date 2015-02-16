dfcd8167519621fc937c1f44f350e06405781a29
Bo Bayles <bbayles@gmail.com>
2015-02-15 18:24:38 -0600

    Clean per flake8, bump version

--- setup.py
+++ setup.py
@@ -1,34 +1,33 @@
 from setuptools import setup, find_packages
-import os
-import sys
 
-long_description = """This project contains library functions for converting\
-  between the Unit Address and MAC address representations of SCTE 55-1\
-  terminals, e.g. Motorola-brand set-top boxes and CableCARDs"""
+long_description = (
+    'This project contains library functions for converting '
+    'between the Unit Address and MAC address representations of SCTE 55-1 '
+    'terminals, e.g. Motorola-brand set-top boxes and CableCARDs.'
+)
 
 setup(
-  name='scte_55-1_address',
-      
-  version='2014.07.26',
+    name='scte_55-1_address',
+    version='2015.2.15',
+    license='MIT',
+    url='https://github.com/bbayles/scte_55-1_address',
 
-  description='SCTE 55-1 terminal identifier converters',
-  long_description="Library for working with SCTE 55-1 terminal\
-    identifiers",
-  
-  url='https://github.com/bbayles/scte_55-1_address',
-  
-  author='Bo Bayles',
-  author_email='bbayles@gmail.com',
-  
-  license='MIT',
-  
-  classifiers=['Development Status :: 5 - Production/Stable',
-               'Intended Audience :: Telecommunications Industry',
-               'Topic :: Utilities',
-               'License :: OSI Approved :: MIT License',
-               'Programming Language :: Python :: 3',],
-  
-  keywords='scte cable settop cablecard',
+    description='SCTE 55-1 terminal identifier converters',
+    long_description=(
+        'Library for working with SCTE 55-1 terminal identifiers'
+    ),
 
-  packages=find_packages(),
+    author='Bo Bayles',
+    author_email='bbayles@gmail.com',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Telecommunications Industry',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='scte cable settop cablecard',
+
+    packages=find_packages(),
 )
--- terminal_address/test_converters.py
+++ terminal_address/test_converters.py
@@ -1,63 +1,62 @@
 # Unit Address / MAC Address converters for SCTE 55-1 terminals
 # Unit tests module
 from .scte_55_1_address import mac_to_ua, ua_to_mac
-import os
 import pkgutil
 import random
 import unittest
 
 
 class ConverterTests(unittest.TestCase):
-  def test_known_ua(self):
-    """mac_to_ua should give known result with known input"""
-    for ua, mac in A_ua_mac:
-      self.assertEqual(ua, mac_to_ua(mac))
+    def test_known_ua(self):
+        """mac_to_ua should give known result with known input"""
+        for ua, mac in A_ua_mac:
+            self.assertEqual(ua, mac_to_ua(mac))
 
-  def test_known_mac(self):
-    """ua_to_mac should give known result with known input"""
-    for ua, mac in A_ua_mac:
-      self.assertEqual(mac, ua_to_mac(ua))
+    def test_known_mac(self):
+        """ua_to_mac should give known result with known input"""
+        for ua, mac in A_ua_mac:
+            self.assertEqual(mac, ua_to_mac(ua))
 
-  def test_known_ua_roundtrip(self):
-    """mac_to_ua(ua_to_mac(ua)) should equal ua for known ua"""
-    for ua, mac in A_ua_mac:
-      self.assertEqual(ua, mac_to_ua(ua_to_mac(ua)))
+    def test_known_ua_roundtrip(self):
+        """mac_to_ua(ua_to_mac(ua)) should equal ua for known ua"""
+        for ua, mac in A_ua_mac:
+            self.assertEqual(ua, mac_to_ua(ua_to_mac(ua)))
 
-  def test_known_mac_roundtrip(self):
-    """ua_to_mac(mac_to_ua(mac)) should equal mac for known mac"""
-    for ua, mac in A_ua_mac:
-      self.assertEqual(mac, ua_to_mac(mac_to_ua(mac)))
+    def test_known_mac_roundtrip(self):
+        """ua_to_mac(mac_to_ua(mac)) should equal mac for known mac"""
+        for ua, mac in A_ua_mac:
+            self.assertEqual(mac, ua_to_mac(mac_to_ua(mac)))
 
-  def test_random_mac_roundtrip(self):
-    """ua_to_mac(mac_to_ua(mac)) should equal mac for random mac"""
-    for _ in range(test_count):
-      n = random.randrange(0, pow(2, 40))
-      mac = format(n, "012x")
-      self.assertEqual(mac, ua_to_mac(mac_to_ua(mac)))
+    def test_random_mac_roundtrip(self):
+        """ua_to_mac(mac_to_ua(mac)) should equal mac for random mac"""
+        for _ in range(test_count):
+            n = random.randrange(0, pow(2, 40))
+            mac = format(n, "012x")
+            self.assertEqual(mac, ua_to_mac(mac_to_ua(mac)))
 
-  def test_invalid_mac(self):
-    for _ in range(test_count):
-      n = random.randrange(pow(2, 40), pow(2, 48))
-      mac = format(n, "012x")
-      self.assertRaises(ValueError, mac_to_ua, mac)
+    def test_invalid_mac(self):
+        for _ in range(test_count):
+            n = random.randrange(pow(2, 40), pow(2, 48))
+            mac = format(n, "012x")
+            self.assertRaises(ValueError, mac_to_ua, mac)
 
-  def test_invalid_mac(self):
-    for _ in range(test_count):
-      n = random.randrange(pow(2, 40), pow(10, 13))
-      s = format(n, "013")
-      ua = "{}-{}-{}-{}".format(s[0: 3], s[3: 8], s[8: 13], "000")
-      self.assertRaises(ValueError, ua_to_mac, ua)
+    def test_invalid_ua(self):
+        for _ in range(test_count):
+            n = random.randrange(pow(2, 40), pow(10, 13))
+            s = format(n, "013")
+            ua = "{}-{}-{}-{}".format(s[0: 3], s[3: 8], s[8: 13], "000")
+            self.assertRaises(ValueError, ua_to_mac, ua)
 
 if __name__ == "__main__":
-  test_count = 1000
-  
-  # Load known values
-  A_ua_mac = []
-  data = pkgutil.get_data(__package__, "mac_ua_tests.csv")
-  data = data.decode("utf-8").splitlines()[1:]
-  for line in data:
-    line = line.strip()
-    ua, mac = line.split(',')
-    A_ua_mac.append((ua, mac))
-  
-  unittest.main()
+    test_count = 1000
+
+    # Load known values
+    A_ua_mac = []
+    data = pkgutil.get_data(__package__, "mac_ua_tests.csv")
+    data = data.decode("utf-8").splitlines()[1:]
+    for line in data:
+        line = line.strip()
+        ua, mac = line.split(',')
+        A_ua_mac.append((ua, mac))
+
+    unittest.main()
